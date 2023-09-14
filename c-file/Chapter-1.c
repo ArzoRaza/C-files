eeb5a5044bba0cbf11389bff603599e3b97a7ba5
Arzoo Raza <142464887+ArzoRaza@users.noreply.github.com>
2023-09-14 23:51:26 +0530

    chapter 1 or chapter 2 more chapter is work on.

---
+++ c-file/Chapter-1.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+
+int main() {
+
+    /*here are integer
+    int num = 786;
+    printf("Enter you num: %d \n", num);
+    */
+
+
+    /*
+    here are flooting 
+    float number = 12.34;
+    printf("Enter your Float Number: %f \n", number);
+    */
+
+
+    /* here are character
+    char star = '*';
+    printf("Enter your star: %c \n", star);
+    */
+
+
+    /* // NOW we see input output
+    int age;
+    printf("Enter your number: ");
+    scanf("%d", & age);
+    printf("Hmara number ho jaiga : %d", age);
+    */
+
+    /* let's write a basic program
+    int a, b; 
+    printf("Enter number of a: ");
+    scanf("%d",&a);   // we use ampersand only in scanf function
+
+    printf("Enter number of b: ");
+    scanf("%d",&b);  // we use ampersand only in scanf function
+
+    // int sum = a + b; 
+    printf("total of sum: %d", a + b);
+    */
+
+    // int side;
+    // printf("Enter num of side: ");
+    // scanf("%d",&side);
+
+    // printf("area of square: %d", side * side );
+
+
+    /* Area of square
+    float side;
+    printf("Enter number of side: ");
+    scanf("%f", & side);
+
+    printf("Area of square is: %f", side * side);
+    */
+
+    // Area of circle 
+
+    float radius;
+    printf("Enter number of radius: ");
+    scanf("%f", &radius);
+
+    printf("Area of circle: %f", 3.14 * radius * radius); 
+    return 0;
+}
---
+++ c-file/chapter-2.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    // valid 
+    // int a = 22;
+    // int b = a;
+    // int c = b + 1; 
+    // int d = 1, e;
+
+
+    // Invalid 
+    // int oldAge = 22;
+    // int newAge = oldAge + years;
+    // int years = 2;
+
+    // int x,y,z = 4; 
+
+
+    // Arithmetic Instructions
+    // valid 
+    // int b, c;
+    // b = c = 9;
+    // int a = b + c;
+    // int power = b^c;
+    // printf("%d", power);
+
+    // modular 
+    // printf("%d", -19%3);
+
+    // type conversion
+    // printf("%d \n", 2 * 2); 
+    // printf("%f \n", 2.0 * 2);
+    // printf("%f \n", 2.0 * 3.0);
+
+    // int a = (int) 1.9999;
+    // printf("%d", a);
+
+    // Associativity (for same precedence)
+    // int x = 3*4/9*3;
+    // int x = 5 * 2 - 3 * 2;
+    // int x = 5 * 2 / 2 * 3;
+    // int x = 5 + 2 / 2 * 3;
+    // printf("%d", x);
+
+    // Relational Operators 
+    /*printf("%d \n", 3 == 3);  // True
+    printf("%d \n", 3 < 3);  // false
+    printf("%d \n", 3 <= 3);  // True
+    printf("%d \n", 3 > 3);  // false
+    printf("%d \n", 3 >= 3);  // True
+    printf("%d \n", 3 != 3);  // false
+    */
+
+    // Logical Operators
+    // printf("%d \n", 4>3 && 7>3); // AND operators True
+    // printf("%d \n", 1>3 || 2>3); // OR operators false
+    // printf("%d \n", !(4>2) );    // NOT operators false
+    // printf("%d \n", !(4>6) && (3<4)); // combination of not, less than
+    
+    // Assignment Operators
+    // int a = 1;
+    // int b = 3; 
+    // a = b;
+    // a += b;
+    // a -= b;
+    // a *= b; 
+    // a %= b;
+    // a /= b;
+    // printf("%d \n", a);
+
+
+    // Write a program to check if a number is divisible by 2 or not.
+    /*int x; 
+    printf("Enter you number");
+    scanf("%d", &x);
+    printf("%d", x % 2 == 0);
+    */
+
+
+    // Write a program to check if a number is odd or even. 
+    // Even means = 1
+    // odd means = 0
+    /*int x; 
+    printf("Enter you number");
+    scanf("%d", &x);
+    printf("%d", x % 2 == 0);
+    */
+
+    // Are the following valid or not?
+    /*printf("%d", 3^3);  // valid
+    int x; y=x;          // valid 
+    int x, y=x;         // Invalid
+    chat stars = '**';  // Invalid
+    */
+
+
+    // Print 1(ture) or 0(false) for following statements:
+    /*int x;
+    printf("Enter a number: ");
+    scanf("%d", & x);
+    printf("%d", x>9 && x<100);
+    */
+    
+    return 0; 
+}
